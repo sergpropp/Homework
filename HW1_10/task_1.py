fc741aaac1e24d74e1046f2c04abebecb5ec1e39
Serg Propp <sergpropp@gmail.com>
2021-04-03 23:23:06 +0500

    task1_3.ready

--- HW1_10/task_1.py
+++ HW1_10/task_1.py
@@ -1,19 +1,17 @@
 class Mentor:
-    def __init__(self, name, surname, gender):
+    def __init__(self, name, surname):
         self.name = name
         self.surname = surname
-        self.gender = gender
-        self.courses_in_progress = []
-        self.grades = {}
         self.courses_attached = []
 
     def __str__(self):
-        return f'Имя: {self.name} \nФамилия: {self.surname}'
+        new_str = f'Имя: {self.name} \nФамилия: {self.surname}\n'
+        return new_str
 
 
-class Review:
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
-        if isinstance(student, Mentor) and course in self.courses_attached and course in student.courses_in_progress:
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
@@ -22,40 +20,95 @@ class Review:
             return 'Ошибка'
 
 
-class Reviewer(Mentor, Review):
-    pass
-
-
-class Student(Mentor, Review):
+class Student:
     def __init__(self, name, surname, gender):
-        super.__init__(self, name, surname, gender)
+        self.name = name
+        self.surname = surname
+        self.gender = gender
         self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+        self.courses_attached = []
+
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in lecturer.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def average(self):
+        listing = []
+        av = 0
+        for values in self.grades.values():
+            for value in values:
+                av += value
+                listing.append(value)
+                continue
+        return int(av / len(listing))
+
+    def __str__(self):
+        return (f'Имя: {self.name}\n'
+                f'Фамилия: {self.surname}\n'
+                f'Средняя оценка за домашние задания: {self.average()}\n'
+                f'Курсы в процессе изучения: {self.courses_in_progress}\n'
+                )
+
+    def add_courses(self, course_name):
+        self.finished_courses.append(course_name)
 
 
-class Lecturer(Mentor):
-    def __init__(self, name, surname, gender):
-        super.__init__(self, name, surname, gender)
+class Lecturer(Mentor, Student):
+    def __init__(self, name, surname):
+        super(Lecturer, self).__init__(name, surname)
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def __str__(self):
+        return (f'Имя: {self.name}\n'
+                f'Фамилия: {self.surname}\n'
+                f'Средняя оценка за лекции: {self.average()}\n'
+                )
 
 
+first_student = Student('Ivan', 'Ivanov', 'man')
+first_student.courses_attached += ['Python']
+first_student.courses_attached += ['Git']
+first_student.courses_in_progress += ['Python']
+first_student.courses_in_progress += ['Git']
 
-best_student = Mentor('Ruoy', 'Eman', 'man')
-best_student.courses_in_progress += ['Python']
+second_student = Student('Masha', 'Ivanova', 'woman')
+second_student.courses_attached += ['Python']
+second_student.courses_attached += ['Git']
+second_student.courses_in_progress += ['Python']
+second_student.courses_in_progress += ['Git']
 
-best_lecturer = Mentor('Some', 'Buddy', 'woman')
-best_lecturer.courses_in_progress += ['Python']
+first_mentor = Reviewer('Karl', 'Sagan')
+first_mentor.courses_attached += ['Python']
+second_mentor = Reviewer('Robert', 'Sapolsky')
+second_mentor.courses_attached += ['Git']
 
-cool_reviewer = Reviewer('Some', 'Buddy', 'woman')
-cool_reviewer.courses_attached += ['Python']
+first_lecturer = Lecturer('Robert', 'Sheckley')
+first_lecturer.courses_in_progress += ['Python']
+second_lecturer = Lecturer('Daria', 'Donzova')
+second_lecturer.courses_in_progress += ['Git']
 
-cool_reviewer.rate_hw(best_lecturer, 'Python', 12)
-cool_reviewer.rate_hw(best_lecturer, 'Python', 13)
+students_rate = [{first_mentor.rate_hw(first_student, 'Python', 9)},
+                 {first_mentor.rate_hw(first_student, 'Python', 10)},
+                 {first_mentor.rate_hw(first_student, 'Python', 8)},
+                 {second_mentor.rate_hw(second_student, 'Git', 6)},
+                 {second_mentor.rate_hw(second_student, 'Git', 5)},
+                 {second_mentor.rate_hw(second_student, 'Git', 4)}
+                 ]
+lecturer_rate = [{first_student.rate_lecturer(second_lecturer, 'Git', 5)},
+                 {second_student.rate_lecturer(second_lecturer, 'Git', 10)},
+                 {first_student.rate_lecturer(first_lecturer, 'Python', 10)},
+                 {second_student.rate_lecturer(first_lecturer, 'Python', 8)}
+                 ]
 
-cool_reviewer.rate_hw(best_student, 'Python', 5)
-cool_reviewer.rate_hw(best_student, 'Python', 6)
-cool_reviewer.rate_hw(best_student, 'Python', 78)
+print(first_student)
+print(first_lecturer)
+print(first_mentor)
 
-print(best_student.grades)
-print(best_lecturer.grades)
-print(best_lecturer)
-print(cool_reviewer)
-print(best_student)
